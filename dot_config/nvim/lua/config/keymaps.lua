733e0cb26506590675f7a761460d56c694805e56
Jed Burrows <jedburrows1997@gmail.com>
2026-06-06 15:16:11 +0100

    feat(vim): fixed issues with vim-tmux-navigator

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Override LazyVim's <C-hjkl> window navigation with vim-tmux-navigator.
+-- These must live here (not in the plugin spec) so they run inside LazyVim's
+-- VeryLazy handler, after LazyVim's own defaults, and win the keymap battle.
+vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { silent = true })
+vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { silent = true })
+vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { silent = true })
+vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { silent = true })
+vim.keymap.set("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", { silent = true })
